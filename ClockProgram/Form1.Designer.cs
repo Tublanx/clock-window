e3d5dbb72478ed1d25625017aa8718bc7fb8c2b9
Tublanx <rlgus03453@naver.com>
2023-01-26 21:45:05 +0900

    남은 시간을 보여주는 label과 reset, plus 버튼 추가

--- ClockProgram/Form1.Designer.cs
+++ ClockProgram/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(159, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Font = new System.Drawing.Font("굴림", 25F);
+            this.plusBtn.Location = new System.Drawing.Point(239, 390);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(66, 38);
+            this.plusBtn.TabIndex = 1;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Webdings", 25F);
+            this.resetBtn.Location = new System.Drawing.Point(167, 390);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(66, 38);
+            this.resetBtn.TabIndex = 1;
+            this.resetBtn.Text = "q";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.Button resetBtn;
     }
 }
 
--- ClockProgram/Form1.cs
+++ ClockProgram/Form1.cs
@@ -16,5 +16,15 @@ namespace ClockProgram
         {
             InitializeComponent();
         }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
