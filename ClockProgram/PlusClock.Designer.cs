28eb54efdb6eed9ebed3c6e04ad977867c60dcbb
Tublanx <rlgus03453@naver.com>
2023-01-30 21:02:17 +0900

    새 타이머 추가 폼 퍼블리싱 완료

--- ClockProgram/PlusClock.Designer.cs
+++ ClockProgram/PlusClock.Designer.cs
@@ -140,7 +140,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(31, 209);
+            this.textBox1.Location = new System.Drawing.Point(31, 205);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(278, 21);
             this.textBox1.TabIndex = 6;
